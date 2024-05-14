f0f3f66c53516a19d83b1f58261fe4fc72a2e817
pukpuk555 <jester2540@gmail.com>
2024-05-14 10:58:50 +0700

    complete

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let dayOfWeek = 2;
+
+if (dayOfWeek === 0) {
+  console.log("Sunday");
+} else if (dayOfWeek === 1) {
+  console.log("Monday");
+} else if (dayOfWeek === 2) {
+  console.log("Tuesday");
+} else if (dayOfWeek === 3) {
+  console.log("Wednesday");
+} else if (dayOfWeek === 4) {
+  console.log("Thursday");
+} else if (dayOfWeek === 5) {
+  console.log("Friday");
+} else if (dayOfWeek === 6) {
+  console.log("Saturday");
+} else {
+  console.log("We only have seven days a week. Create your day");
+}
